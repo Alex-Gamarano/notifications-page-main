ce7d1a0fde0c50b25c029fda6d371909286cfee3
Alex-Gamarano <145658322+Alex-Gamarano@users.noreply.github.com>
2024-03-12 15:02:10 +0100

    reset function optimized

--- script.js
+++ script.js
@@ -12,8 +12,11 @@ let counter = document.querySelector(".counter");
 let circles = document.querySelectorAll(".circle");
 let i = circles.length;
 let wrappers = document.querySelectorAll(".wrapper");
+
 let reset = document.querySelector(".reset").addEventListener("click", () => {
   circles.forEach((circle) => {
+    circle.parentElement.parentElement.parentElement.style.backgroundColor =
+      "var(--white)";
     circle.classList.remove("circle");
   });
   i = 0;
@@ -26,6 +29,8 @@ wrappers.forEach((wrapper) => {
   wrapper.addEventListener("click", () => {
     if (wrapper.lastElementChild.classList.contains("circle")) {
       wrapper.lastElementChild.classList.remove("circle");
+      wrapper.parentElement.parentElement.style.backgroundColor =
+        "var(--white)";
       wrapper.removeEventListener("click", this);
       i--;
       console.log(i);
