49389bfdfc9934676ce953e682677e48bccfb782
Alex-Gamarano <145658322+Alex-Gamarano@users.noreply.github.com>
2024-03-12 14:36:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+/* document.querySelectorAll(".post").forEach((post) => {
+  post.addEventListener("dblclick", () => {
+    post.lastElementChild.firstElementChild.lastElementChild.classList.add(
+      "circle"
+    );
+    i++;
+    console.log(i);
+    counter.innerHTML = i;
+  });
+}); */
+let counter = document.querySelector(".counter");
+let circles = document.querySelectorAll(".circle");
+let i = circles.length;
+let wrappers = document.querySelectorAll(".wrapper");
+let reset = document.querySelector(".reset").addEventListener("click", () => {
+  circles.forEach((circle) => {
+    circle.classList.remove("circle");
+  });
+  i = 0;
+  counter.innerHTML = 0;
+});
+
+counter.innerHTML = i;
+
+wrappers.forEach((wrapper) => {
+  wrapper.addEventListener("click", () => {
+    if (wrapper.lastElementChild.classList.contains("circle")) {
+      wrapper.lastElementChild.classList.remove("circle");
+      wrapper.removeEventListener("click", this);
+      i--;
+      console.log(i);
+      counter.innerHTML = i;
+    }
+  });
+});
